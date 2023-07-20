cf92bddd641af586cdaacf5c361d821dabc9683d
shoaibakhterqadri <github@4dmagic.pk>
2023-07-20 18:42:05 +0500

    Template-Reference variable

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,33 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls:['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
-  // Simple Type
-  data:number=20;
-  getData(item:number | boolean){
-    if(typeof item==="number"){
-      return item*this.data;
-    }
-  } 
-
-  // Object
-  dataObj:{name:string,phone:number}={name:"shoaib",phone:213};
-  getDataObj(item:{name:string,phone:number}){
-    if(typeof item==="number"){
-      return item*this.data;
-    }
-  } 
-
-
-  // Array
-  dataArray:string[]=['1','2'];
-  getDataArray(item:number[]){
-    if(typeof item==="number"){
-      return item*this.data;
-    }
-  } 
-
+  data: string = '';
+  getValue(item: any) {
+    this.data = item;
+    console.log(item);
+  }
 }
